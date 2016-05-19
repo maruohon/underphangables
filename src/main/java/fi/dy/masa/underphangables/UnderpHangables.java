5a0cc82310652edc5e440499f58a42f0cbf5a803
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-05-19 15:18:19 +0300

    Update to Minecraft 1.9.4

--- src/main/java/fi/dy/masa/underphangables/UnderpHangables.java
+++ src/main/java/fi/dy/masa/underphangables/UnderpHangables.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
-    acceptableRemoteVersions="*", acceptedMinecraftVersions = "1.9")
+    acceptableRemoteVersions="*", acceptedMinecraftVersions = "1.9,1.9.4")
 public class UnderpHangables
 {
     @Instance(Reference.MOD_ID)
